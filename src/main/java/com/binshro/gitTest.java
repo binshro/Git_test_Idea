9d9b8f504284b8e9128b1027ad2d25d06d926c2c
Binshro <binshengrong@gmail.com>
2022-11-08 21:24:02 +0800

    second commit

--- src/main/java/com/binshro/gitTest.java
+++ src/main/java/com/binshro/gitTest.java
@@ -3,5 +3,6 @@ package com.binshro;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git Idea");
+        System.out.println("hello git Idea2");
     }
 }
